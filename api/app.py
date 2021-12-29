ea97a7228c4b4bad867455c5dd0d574e8bbe9f00
willson <marc@manthano.ai>
2021-12-30 00:43:25 +0100

    python backend will be removed

--- api/app.py
+++ api/app.py
@@ -10,66 +10,44 @@ app = Flask(__name__)
 
 API_BASE = "http://alephium:12973"
 
-@app.route('/api/wallets', methods=['GET'])
+
+@app.route("/api/wallets", methods=["GET"])
 def wallets():
-    wallets = requests.get(f'{API_BASE}/wallets')
-    print(wallets.text)
-    return Response(wallets.text,200)
+    wallets = requests.get(f"{API_BASE}/wallets")
+    return Response(wallets)
 
-@app.route('/api/unlock',methods=['POST'])
-def unlock_wallet_yes():
-    name = request.args.get('walletName')
-    password = request.args.get('wpassword')
-    print(name, password)
 
+@app.route("/api/unlock", methods=["POST"])
+def unlock_wallet():
+    name = request.args.get("walletName")
+    password = request.args.get("wpassword")
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    wallet = requests.post(f'{API_BASE}/wallets/{name}/unlock', headers=headers,json={"password": password})
+    wallet = requests.post(
+        f"{API_BASE}/wallets/{name}/unlock",
+        headers=headers,
+        json={"password": password},
+    )
     if wallet.ok:
-       return Response("wallet unlocked...",200)
+        return Response(wallet)
     else:
-        return Response("wrong password...",404)
+        return Response(status=500)
 
 
-@app.route('/')
-def compileForm():
+@app.route("/api/contracts/compile-contract", methods=["POST"])
+def compile_contract():
+    code = "this is it"
+    url = "{API_BASE}/api/contracts/compile-contract"
+    payload = f"{code}"
+    headers = {"Content-Type": "application/json"}
 
-    return Response("What is happening", 200)
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-
-@app.route('/', methods=['POST'])
-def compile():
-    text = request.form['text']
-    print(text)
-    data = {'code': text}
-    headers = {"Content-Type": "application/json; charset=utf-8"}
-
-    r = requests.post(f'{API_BASE}/contracts/compile-contract', json=data, headers=headers)
-    if r.ok:
-        if r.json() is not None and r.json().get('code'):
-            print("Contract compiled")
-
-            pubKey = getPublicKeyActiveAddress()
-            code = r.json()['code']
-            parameters = f"[@{address},10000000000000000000000000000u]"
-
-            unsignedContract = buildContract(pubKey, code, parameters)
-
-            if unsignedContract.ok:
-                hash = unsignedContract.json().get('hash')
-                if len(hash) > 0:
-                    signature = signContract(hash)
-
-                    if len(signature) > 0:
-                        print(unsignedContract.json())
-                        unsignedTx = unsignedContract.json().get('unsignedTx')
-                        signedContract = submitContract(unsignedTx, signature)
-                        print(signedContract)
-                return signedContract
-
-    return f'Error: {r.content}'
+    print(response.text)
 
 
-def buildContract(pubKey, code, state, gas=1000000, issueTokenAmmount=10000000000000000000000000000):
+def buildContract(
+    pubKey, code, state, gas=1000000, issueTokenAmmount=10000000000000000000000000000
+):
     headers = {"Content-Type": "application/json; charset=utf-8"}
 
     data = {
@@ -77,36 +55,45 @@ def buildContract(pubKey, code, state, gas=1000000, issueTokenAmmount=1000000000
         "code": code,
         "gas": gas,
         "state": state,
-        "issueTokenAmount": str(issueTokenAmmount)
+        "issueTokenAmount": str(issueTokenAmmount),
     }
 
-    build = requests.post(f'{API_BASE}/contracts/build-contract', json=data, headers=headers)
+    build = requests.post(
+        f"{API_BASE}/contracts/build-contract", json=data, headers=headers
+    )
 
     print("Contract build")
 
-    if build.ok and build.json().get('unsignedTx'):
+    if build.ok and build.json().get("unsignedTx"):
         return build
 
 
 def getWalletAddresses():
     headers = {"Content-Type": "application/json; charset=utf-8"}
 
-    wallets = requests.get(f'{API_BASE}/wallets', headers=headers)
+    wallets = requests.get(f"{API_BASE}/wallets", headers=headers)
 
     if wallets.ok:
         for wallet in wallets.json():
-            if wallet.get('locked'):
-                return False,False,False
-                #requests.post(f'{API_BASE}/wallets/{wallet["walletName"]}/unlock', headers=headers,json={"password": "test"})
+            if wallet.get("locked"):
+                return False, False, False
+                # requests.post(f'{API_BASE}/wallets/{wallet["walletName"]}/unlock', headers=headers,json={"password": "test"})
 
-            walletName = wallet.get('walletName')
-            address = requests.get(f'{API_BASE}/wallets/{walletName}/addresses', headers=headers)
+            walletName = wallet.get("walletName")
+            address = requests.get(
+                f"{API_BASE}/wallets/{walletName}/addresses", headers=headers
+            )
 
             # Not select the miner wallet
-            if address.json().get('activeAddress') and len(address.json().get('addresses')) == 1:
-                balance = requests.get(f'{API_BASE}/addresses/{address.json().get("activeAddress")}/balance',
-                                       headers=headers).json()['balanceHint']
-                return walletName, address.json()['activeAddress'], balance
+            if (
+                address.json().get("activeAddress")
+                and len(address.json().get("addresses")) == 1
+            ):
+                balance = requests.get(
+                    f'{API_BASE}/addresses/{address.json().get("activeAddress")}/balance',
+                    headers=headers,
+                ).json()["balanceHint"]
+                return walletName, address.json()["activeAddress"], balance
 
     return False, False, False
 
@@ -114,41 +101,46 @@ def getWalletAddresses():
 def getPublicKeyActiveAddress():
     headers = {"Content-Type": "application/json; charset=utf-8"}
     walletName, address, balance = getWalletAddresses()
-    return requests.get(f'{API_BASE}/wallets/{walletName}/addresses/{address}', headers=headers).json().get('publicKey')
+    return (
+        requests.get(
+            f"{API_BASE}/wallets/{walletName}/addresses/{address}", headers=headers
+        )
+        .json()
+        .get("publicKey")
+    )
 
 
 def signContract(hash):
-    print('signContract')
+    print("signContract")
     headers = {"Content-Type": "application/json; charset=utf-8"}
 
-    data = {
-        "data": hash
-    }
+    data = {"data": hash}
 
-    contract = requests.post(f'{API_BASE}/wallets/{walletName}/sign', headers=headers, json=data)
+    contract = requests.post(
+        f"{API_BASE}/wallets/{walletName}/sign", headers=headers, json=data
+    )
 
-    if contract.ok and contract.json().get('signature'):
-        return contract.json().get('signature')
+    if contract.ok and contract.json().get("signature"):
+        return contract.json().get("signature")
     else:
         return "Error on signContract"
 
 
 def submitContract(unsignedTx, signature):
-    print('submitContract')
+    print("submitContract")
     headers = {"Content-Type": "application/json; charset=utf-8"}
 
-    data = {
-        "unsignedTx": unsignedTx,
-        "signature": signature
-    }
+    data = {"unsignedTx": unsignedTx, "signature": signature}
 
-    contract = requests.post(f'{API_BASE}/transactions/submit', headers=headers, json=data)
+    contract = requests.post(
+        f"{API_BASE}/transactions/submit", headers=headers, json=data
+    )
 
-    if contract.ok and contract.json().get('txId'):
-        return contract.json().getId('txId')
+    if contract.ok and contract.json().get("txId"):
+        return contract.json().getId("txId")
     else:
         print(contract.content)
-        print(contract.json().get('detail'))
+        print(contract.json().get("detail"))
         return f"Error on submitContract: {contract.json().get('detail')}"
 
 
--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,11 +1,11 @@
-reload=True
+reload = True
 # https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
-bind = ':5000'
+bind = ":5001"
 # Use one worker for GPU models
 workers = 1
 threads = 1
 # You can use multiple threads
 timeout = 1500
-errorlog = '-'
-loglevel = 'info'
-accesslog = '-'
+errorlog = "-"
+loglevel = "info"
+accesslog = "-"
